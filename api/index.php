0ab30517bd1bad77cd84d1644be4d04f2edae486
Leousco <leovillete878@gmail.com>
2026-09-01 14:55:08 +0800

    deploy fix

--- api/index.php
+++ api/index.php
@@ -4,5 +4,20 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->safeLoad();
 
-require __DIR__ . '/../views/public/main.php';
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$path = rtrim($path, '/');
+
+// Homepage
+if ($path === '' || $path === '/index.php') {
+    require __DIR__ . '/../views/public/main.php';
+    exit;
+}
+
+$file = __DIR__ . '/../views' . $path . '.php';
+
+if (file_exists($file)) {
+    require $file;
+} else {
+    require __DIR__ . '/../views/public/unauthorized.php'; // or a real 404 page if you have one
+}
 exit;
